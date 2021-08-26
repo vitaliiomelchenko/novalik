1f46bde183c8fad95631ee2c48439acc0de4a72c
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-08-26 10:36:58 +0300

    Create contactsBlock.php

---
+++ acf-blocks/contactsBlock.php
@@ -0,0 +1,48 @@
+<?php 
+	$contactUsBlockForm = get_sub_field('cta_form_shortcode');
+?>
+<section class="contactsPage">
+	<div class="container">
+		<h1><?php the_title(); ?></h1>
+
+		<div class="contactsPage__items">
+			<div class="row align-items-start justify-content-between">
+				<?php
+				if( have_rows('contacts_repeater') ):
+
+				    while ( have_rows('contacts_repeater') ) : the_row();
+						$contacts_repeater_icon = the_sub_field('contacts_repeater_icon');
+				        ?>
+				        	<div class="mb-3 mb-lg-0 сol-12 col-sm-6 col-md-4 col-lg-3">
+				        		<div class="item">
+				        			<?php if( !empty( $contacts_repeater_icon ) ): ?>
+				        				<div class="icon">
+				        					<img src="<?php echo esc_url($contacts_repeater_icon['url']); ?>" alt="">
+				        				</div>
+				        			<?php else :?>
+				        			<?php endif; ?>
+				        			
+				        			<div class="text">
+				        				<?php the_sub_field('contacts_repeater_text'); ?>
+				        			</div>
+				        		</div>
+				        	</div>
+				        <?php
+
+				    endwhile;
+
+				else :
+
+				endif;
+
+				?>
+			</div>
+		</div>
+		<div class="contactsPage__cta">
+			<?php echo do_shortcode( $contactUsBlockForm ); ?>
+		</div>
+	</div>
+	<div class="contactsPage__map">
+		<?php the_sub_field('map'); ?>
+	</div>
+</section>
